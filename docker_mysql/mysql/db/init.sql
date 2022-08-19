4b04e972fdf271f0f948a66f846eae65c657d71c
haru <haruki1472@icloud.com>
2022-08-19 23:35:32 +0900

    DB修正

--- docker_mysql/mysql/db/init.sql
+++ docker_mysql/mysql/db/init.sql
@@ -1,10 +1,14 @@
-CREATE DATABASE sample_db;
+CREATE DATABASE t_customer;
 use sample_db;
 
 CREATE TABLE users (
   id int(10) unsigned not null auto_increment,
-  name varchar(255) not null,
-  created_time datetime not null default current_timestamp,
+  firstName varchar(255) not null,
+  lastName varchar(255) not null,
+  mail varchar(255) not null,
+  phoneNumber varchar(255) not null,
+  user datetime not null default current_timestamp,
+  varchar varchar(255) not null,
   updated_time datetime not null default current_timestamp on update current_timestamp,
   primary key (id)
 );
